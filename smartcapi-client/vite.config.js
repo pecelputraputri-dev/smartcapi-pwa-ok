9afe292f283d8f5d292645fb27bc22a31444647f
pecelputraputri-dev <pecel.putraputri@gmail.com>
2025-10-24 19:49:17 +0700

    Add files via upload

---
+++ smartcapi-client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      // Proxying API requests to the backend server
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
